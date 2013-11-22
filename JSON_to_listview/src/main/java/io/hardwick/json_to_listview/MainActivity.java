d674f76fed48a59507fc915bec5b7e750f9d0f0c
Matt Hardwick <matthew.hardwick@gmail.com>
2013-11-21 23:11:45 -0800

    Added Simple AsyncHttpClient, and JSON Parser to Object

--- JSON_to_listview/src/main/java/io/hardwick/json_to_listview/MainActivity.java
+++ JSON_to_listview/src/main/java/io/hardwick/json_to_listview/MainActivity.java
@@ -2,9 +2,16 @@ package io.hardwick.json_to_listview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +20,7 @@ public class MainActivity extends Activity {
     List<SimpleObject> items;
     ListView listView;
     ArrayAdapter<SimpleObject> adapter;
+    JSONArray jArray;
 
     public String json = "https://raw.github.com/matthewhardwick/watts_input_to_json/master/someP1.lib0.json";
 
@@ -21,20 +29,79 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup Items Container
         items = new ArrayList<SimpleObject>();
-        for (int i = 0; i < 25; ++i) {
-            items.add(new SimpleObject("Name", "Type"));
-        }
 
+        // Setup Listview, yes items is empty, that is OK
         listView = (ListView) findViewById(R.id.listView);
+        // NOTE: It is OK to use a SimpleObject, with an android.R.layout.simple_list_item_1 because
+        // SimpleObject has a toString method declared, which the adapter calls to generate the
+        // ListView text.
         adapter = new ArrayAdapter<SimpleObject>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
+
+        // Now that the adapter is setup, if we need to update the listView we will use adapter.add
+        // instead of items.add, the adapter will take care of adding to the host container.
+        // Why did we do it this way? Because the HttpClient is Asynchronous, meaning it is run
+        // on a separate thread from the UI, so the Activity continues to run at the same time
+        // as the HttpClient fetches the Data.
+
+        // Setup the AsyncHttpClient to fetch the JSON String
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Spin up a new thread, and fetch the JSON
+        client.get(json, new AsyncHttpResponseHandler() {
+
+            // When the JSON has been fetched, we will process it
+            // The JSON is simply a string at this point. Now because the JSON is formated as a
+            // JSON Array, we will parse it as an Array, and then loop over each JSON Object
+            // Fetch that object, and parse out two values from it, and put them into our
+            // Simple Object Class.
+            @Override
+            public void onSuccess(String response) {
+                try {
+                    jArray = new JSONArray(response); // Parse JSON String to JSON Array
+                    for (int i = 0; i < jArray.length(); ++i) { // Loop over Array
+                        SimpleObject simpleObject = new SimpleObject(); // Create a new Simple Object
+
+                        JSONObject jObject = jArray.getJSONObject(i); // Fetch the ith JSON Object
+                        // from the JSON Array
+                        simpleObject.setName(jObject.getString("Name")); // Parse Name from the JSON
+                        // Object, and put into our
+                        // object
+                        simpleObject.setType(jObject.getString("Type")); // Do the same for type
+
+                        // Remember there are other items in the JSON Object, and they are of other
+                        // Types, so you might want to switch based on type and create an object
+                        // such as a Book, Periodical, or Member
+
+                        // For Reference to get the Checked out Array of a Member to a List
+                        if (simpleObject.getType().equals("Member")) {
+                            List<String> checkedOut = new ArrayList<String>();
+                            JSONArray checkedOutArray = jObject.getJSONArray("Checked_Out");
+                            for (int j = 0; j < checkedOutArray.length(); ++j)
+                                checkedOut.add(checkedOutArray.getString(i));
+                            // Do something with the List, such as put it in a Member Object for later
+                        }
+
+                        // Add an Item to the Adapter, which will add it to the items List, and
+                        // update the List View
+                        adapter.add(simpleObject);
+                    }
+                    Log.d("List Size", Integer.toString(items.size()));
+                } catch (JSONException e) {
+                    Log.d("JSON Parse", e.toString());
+                }
+            }
+        });
 
     }
 
     public class SimpleObject {
         public String name;
         public String type;
+
+        public SimpleObject() {
+        }
 
         public SimpleObject(String name, String type) {
             this.name = name;
@@ -59,6 +126,7 @@ public class MainActivity extends Activity {
 
         @Override
         public String toString() {
+            // Example of a String Builder like we talked about in class.
             StringBuilder builder = new StringBuilder();
             return builder.append(name).append(", ").append(type).toString();
         }
